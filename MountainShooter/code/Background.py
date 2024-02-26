993460c7f86414d3f44d62bc52da1adeac31741a
luanmnogueira <luanmnog@outlook.com>
2024-02-26 16:42:00 -0300

    Text Menu

---
+++ MountainShooter/code/Background.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from Entity import Entity
+from Background import Background
+
+class Background(Entity, Background):
+    def __init__(self):
+        pass
+
+    def move(self, ):
+        pass
+
